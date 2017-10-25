bbd086104960945c3c6bdb56cb9687b07fe804e6
MayraLiz <mayra_liz4@outlook.com>
2017-10-24 21:27:46 -0500

    inicio del proyecto

---
+++ noticias/backend/views/site/index.php
@@ -0,0 +1,31 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'ITVER';
+?>
+<div class="site-index">
+
+	<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="http://radiotec.itver.edu.mx/images/mision.jpg?crc=14604690" alt="First slide">
+    </div>
+  </div>
+</div>
+
+
+    <div class="jumbotron">
+        <h1>¡Bievenido!</h1>
+
+        <p class="lead">Al portal de noticias del ITVER</p>
+        <p class="lead">Creado por estudiantes para estudiantes.</p>
+
+    </div>
+
+    <div class="body-content">
+
+
+
+    </div>
+</div>
